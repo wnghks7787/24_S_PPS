437d1da6c7f2cfad615ecc0bde72a64875d740a2
wnghks7787 <wnghks7787@naver.com>
2024-07-16 02:08:49 +0900

    update A023

---
+++ Week3/0715/A023_임주환_240715.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int answer = num;
+        while(answer >= 10)
+        {
+            answer = 0;
+            while(num > 0)
+            {
+                answer += (num % 10);
+                num /= 10;
+            }
+            num = answer;
+        }
+
+        return answer;
+    }
+};
